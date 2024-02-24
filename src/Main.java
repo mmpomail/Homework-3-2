3a85e180c1f431379a67c5ba428d124b4c0fc009
MailMan <ainddal@gmail.com>
2024-02-24 13:40:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+         BmiService service = new BmiService();
+         float heightMeters = 1.73F; // рост человека в метрах
+         int massKG = 73; // масса тела человека в килограммах
+         System.out.println("Ваш индекс массы тела = " + service.calculate (heightMeters,massKG));
+    }
+}
